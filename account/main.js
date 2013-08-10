c234b9767ded9acbd96abd6d275aee821833889a
Paresh Shah <paresh.d.s@gmail.com>
2013-08-10 12:29:03 +0530

    Renamed angular-auth-demo to angularAuthApp.

--- account/main.js
+++ account/main.js
@@ -1,4 +1,4 @@
-angular.module('angular-auth-demo', ['http-auth-interceptor','accountControllerMock'])
+angular.module('angularAuthApp', ['http-auth-interceptor','accountControllerMock'])
   /**
    * This directive will find itself inside HTML as a class,
    * and will remove that class, so CSS will remove loading image and show app content.
