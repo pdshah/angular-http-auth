3a2e114ac1a8ef2987b3d9932b4d34d433f4c44f
Paresh Shah <paresh.d.s@gmail.com>
2013-08-22 17:02:16 +0530

    Updated the function declaration convention.

--- app/account/account-controller.js
+++ app/account/account-controller.js
@@ -1,34 +1,35 @@
-var app = angular.module('angularAuthApp');
-app.controller('AccountCtrl', ['$scope', '$http', 'authService', function ($scope, $http, authService) {
+(function () {
     'use strict';
+    var app = angular.module('angularAuthApp');
+    app.controller('AccountCtrl', ['$scope', '$http', 'authService', function ($scope, $http, authService) {
+        $scope.publicContent = [];
+        $scope.restrictedContent = [];
 
-    $scope.publicContent = [];
-    $scope.restrictedContent = [];
+        this.authenticate = function (loginModel) {
+            $http.post('auth/login', loginModel).success(function () {
+                authService.loginConfirmed();
+            });
+        };
 
-    this.authenticate = function (loginModel) {
-        $http.post('auth/login', loginModel).success(function () {
-            authService.loginConfirmed();
-        });
-    };
+        this.publicAction = function () {
+            $http.post('data/public', $scope.publicData).success(function (response) {
+                $scope.publicContent.push(response);
+            });
+        };
 
-    this.publicAction = function () {
-        $http.post('data/public', $scope.publicData).success(function (response) {
-            $scope.publicContent.push(response);
-        });
-    };
+        this.restrictedAction = function () {
+            $http.post('data/protected', $scope.restrictedData).success(function (response) {
+                // this piece of code will not be executed until user is authenticated
+                $scope.restrictedContent.push(response);
+            });
+        };
 
-    this.restrictedAction = function () {
-        $http.post('data/protected', $scope.restrictedData).success(function (response) {
-            // this piece of code will not be executed until user is authenticated
-            $scope.restrictedContent.push(response);
-        });
-    };
-
-    this.logout = function () {
-        $http.post('auth/logout').success(function () {
-            $scope.restrictedContent = [];
-        });
-    };
-    $scope.AccountCtrl = this;
-    return $scope.AccountCtrl;
-}]);
+        this.logout = function () {
+            $http.post('auth/logout').success(function () {
+                $scope.restrictedContent = [];
+            });
+        };
+        $scope.AccountCtrl = this;
+        return $scope.AccountCtrl;
+    }]);
+}());
