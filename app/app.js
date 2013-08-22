6832ac21615a698b57df4ff5b76e195d57748196
Paresh Shah <paresh.d.s@gmail.com>
2013-08-22 17:07:23 +0530

    Corrected controler name typo.

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@
 var app = angular.module('angularAuthApp', ['httpAuthInterceptor']);
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
-        when('/', { 'templateUrl' : 'account/login.tpl.html'}).
-        when('/public', { 'templateUrl' : 'public/data.tpl.html' });
+        when('/', { 'templateUrl' : 'account/login.tpl.html', 'controller' : 'AccountCtrl' }).
+        when('/public', { 'templateUrl': 'public/data.tpl.html', 'controller': 'AccountCtrl' });
 }]);
 }());
