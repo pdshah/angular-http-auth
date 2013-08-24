07a269d4e3452c25598332c8d35c3b554e771e45
Paresh Shah <paresh.d.s@gmail.com>
2013-08-24 13:58:02 +0530

    updated code formatting.

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
 (function () {
-'use strict';
-var app = angular.module('angularAuthApp', ['httpAuthInterceptor']);
-app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.
-        when('/', { 'templateUrl' : 'account/login.tpl.html', 'controller' : 'AccountCtrl' }).
-        when('/public', { 'templateUrl': 'public/data.tpl.html', 'controller': 'AccountCtrl' });
-}]);
+    'use strict';
+    angular.module('angularAuthApp', ['httpAuthInterceptor']).
+        config(['$routeProvider', function ($routeProvider) {
+            $routeProvider.
+                when('/', { 'templateUrl' : 'account/login.tpl.html', 'controller' : 'AccountCtrl' }).
+                when('/public', { 'templateUrl': 'public/data.tpl.html', 'controller': 'AccountCtrl' });
+        }]);
 }());
