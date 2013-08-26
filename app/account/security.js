81080e41630bf6d524c7c34500ed291794b0c570
Paresh Shah <paresh.d.s@gmail.com>
2013-08-26 17:10:13 +0530

    Updated page redirection logic.

--- app/account/security.js
+++ app/account/security.js
@@ -1,13 +1,14 @@
 (function () {
     'use strict';
     var app = angular.module('angularAuthApp');
-    app.controller('AccountCtrl', ['$scope', '$http', 'authService', function ($scope, $http, authService) {
+    app.controller('AccountCtrl', ['$scope', '$http', '$location', 'authService', function ($scope, $http, $location, authService) {
         $scope.publicContent = [];
         $scope.restrictedContent = [];
 
         this.authenticate = function (loginModel) {
             $http.post('auth/login', loginModel).success(function () {
                 authService.loginConfirmed();
+                $location.path('/private');
             });
         };
 
@@ -31,25 +32,19 @@
         };
         $scope.AccountCtrl = this;
         return $scope.AccountCtrl;
-    }]);
+    } ]);
 
-    /**
-    * This directive will find itself inside HTML as a class,
-    * and will remove that class, so CSS will remove loading image and show app content.
-    * It is also responsible for showing/hiding login form.
-    */
     app.directive('restricted', ['$location', function ($location) {
         return {
             restrict: 'A',
-            link: function (scope, elem, attrs) {
+            link: function (scope, element, attrs) {
                 scope.$on('event:auth-loginRequired', function () {
-                    scope.$apply(function () { $location.path("/route"); });
+                    $location.path('/');
                 });
                 scope.$on('event:auth-loginConfirmed', function () {
-                    main.show();
-                    login.slideUp();
+                    $location.path('/private');
                 });
             }
-        }
+        };
     }]);
 }());
